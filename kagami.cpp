10e20ddb1eb3c6eb364c08982738f8e4dc6fbc3e
Matt Moore <matt@mattmoore.io>
2020-03-30 16:49:56 -0500

    Reformat.

--- kagami.cpp
+++ kagami.cpp
@@ -5,14 +5,18 @@ Kagami::Kagami()
 }
 
 void Kagami::screenshot() {
-    CGImageRef screenShot = CGWindowListCreateImage( CGRectInfinite,
-    kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageDefault);
+    CGImageRef screenShot = CGWindowListCreateImage(
+                CGRectInfinite,
+                kCGWindowListOptionOnScreenOnly,
+                kCGNullWindowID,
+                kCGWindowImageDefault
+                );
 
     CFStringRef file = CFSTR("/Users/mattmoore/Desktop/lala.jpg");
     CFStringRef type = CFSTR("public.jpeg");
-    //CFURLRef urlRef = CFURLCreateWithString( NULL, file, NULL );
-    CFURLRef urlRef = CFURLCreateWithFileSystemPath( kCFAllocatorDefault, file, kCFURLPOSIXPathStyle, false );
-    CGImageDestinationRef idst = CGImageDestinationCreateWithURL( urlRef, type, 1, NULL );
-    CGImageDestinationAddImage( idst, screenShot, NULL );
-    CGImageDestinationFinalize( idst );
+    //CFURLRef urlRef = CFURLCreateWithString(NULL, file, NULL);
+    CFURLRef urlRef = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, file, kCFURLPOSIXPathStyle, false);
+    CGImageDestinationRef idst = CGImageDestinationCreateWithURL(urlRef, type, 1, NULL);
+    CGImageDestinationAddImage(idst, screenShot, NULL);
+    CGImageDestinationFinalize(idst);
 }
